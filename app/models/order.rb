693100caeee74c6c02400930989dfee8e89f4c36
dzhiqin <dzhiqin@126.com>
2018-04-24 10:09:24 +0800

    product_list

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
   validates_presence_of :billing_name,:billing_address,:shipping_name,:shipping_address
+  has_many :product_lists
+  before_create :generate_token
+  def generate_token
+    self.token=SecureRandom.uuid
+  end
 end
